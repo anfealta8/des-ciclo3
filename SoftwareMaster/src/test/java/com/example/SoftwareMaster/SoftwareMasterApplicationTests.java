2044c6754ee8753f0ec5f3f3e0f3589288c2ca51
duqc05 <andreslabi62@gmail.com>
2022-08-28 10:39:44 -0500

    primer commit

---
+++ SoftwareMaster/src/test/java/com/example/SoftwareMaster/SoftwareMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoftwareMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
