839872adaa573fca8e6cc61ec3e4aba53ce6e4db
duqc05 <andreslabi62@gmail.com>
2022-08-28 11:01:48 -0500

    clase empresa

---
+++ SoftwareMaster/src/main/java/com/example/SoftwareMaster/empresa.java
@@ -0,0 +1,52 @@
+package com.example.SoftwareMaster
+
+
+public class empresa{
+
+    private String nombre;
+
+    private String direccion;
+
+    private int telefono;
+
+    private int nit;
+
+    public empresa(String nombre, String direccion, int telefono, int nit) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+}
